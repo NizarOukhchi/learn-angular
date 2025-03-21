8389ed4519d337dd52f6b300729926c10ac36c2f
houssam <hradi2346@gmail.com>
2025-03-21 00:44:55 +0000

    Added HomeListComponent and related updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomecardComponent } from './homecard/homecard.component';
 import { HomeListComponent } from './home-list/home-list.component';
 
 export const routes: Routes = [
-  {
-    path: 'homes',
-    component: HomeListComponent,
-  },
-  {
-    path: '',
-    redirectTo: 'homes',
-    pathMatch: 'full',
-  },
+    {
+        path: "",
+        redirectTo: "homes",
+        pathMatch: "full",
+      },
+      {
+        path: "homes",
+        component: HomeListComponent,
+      },
+      {
+        path: "card",
+        component: HomecardComponent,
+      },
+
 ];
+
